c622c70f0e4f5e9b13c3eb736ff98d3c509d24f3
root <root@localhost.localdomain>
2019-08-04 13:23:18 -0400

    Ex

---
+++ Ex.java
@@ -0,0 +1,27 @@
+package geometry;
+
+public class Ex {
+	
+	
+	enum WeekDay {
+		
+		Monday,
+		Tuesday,
+		Wednesday,
+		Thursday,
+		Friday;
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			
+		}
+		//int var3 = (condition)? value : value2; //syntax
+		// boolean truth = (a > b) ? true : false;
+	
+	static {
+		System.out.println("Hello World !");
+	}
+}
+	
